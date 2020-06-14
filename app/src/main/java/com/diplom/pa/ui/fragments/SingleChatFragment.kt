9940080a727f9753b668c2c3e8cc829251cedb26
Akrid <Akrid782@mail.ru>
2020-06-14 15:52:31 +0300

    Обновление информации в toobar в одиночном чате

--- app/src/main/java/com/diplom/pa/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/com/diplom/pa/ui/fragments/SingleChatFragment.kt
@@ -1,20 +1,42 @@
 package com.diplom.pa.ui.fragments
 
+import android.view.View
 import com.diplom.pa.R
 import com.diplom.pa.models.CommonModel
-import com.diplom.pa.utility.APP_ACTIVITY
-import android.view.View
+import com.diplom.pa.models.User
+import com.diplom.pa.utility.*
+import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.toolbar_info.view.*
 
-class SingleChatFragment(contact: CommonModel) : BaseFragment(R.layout.fragment_single_chat) {
+class SingleChatFragment(private val contact: CommonModel) :
+    BaseFragment(R.layout.fragment_single_chat) {
+
+    private lateinit var mListenerInfoToolbar: AppValueEventListener
+    private lateinit var mReceivingUser: User
+    private var mToolbarInfo: View = APP_ACTIVITY.mToolbar.toolbar_info
+    private lateinit var mRefUser: DatabaseReference
 
     override fun onResume() {
         super.onResume()
-        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.VISIBLE
+        mToolbarInfo.visibility = View.VISIBLE
+        mListenerInfoToolbar = AppValueEventListener {
+            mReceivingUser = it.getUserModel()
+            initInfoToolbar()
+        }
+        mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
+        mRefUser.addValueEventListener(mListenerInfoToolbar)
+    }
+
+    private fun initInfoToolbar() {
+        mToolbarInfo.toolbar_chat_image.downloadAndSetImage(mReceivingUser.photoUrl)
+        mToolbarInfo.toolbar_contact_chat_fullname.text = mReceivingUser.fullname
+        mToolbarInfo.toolbar_contact_chat_status.text = mReceivingUser.state
     }
 
     override fun onPause() {
         super.onPause()
-        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.GONE
+        mToolbarInfo.visibility = View.GONE
+        mRefUser.removeEventListener(mListenerInfoToolbar)
     }
 }
